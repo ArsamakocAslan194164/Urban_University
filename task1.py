c18b7e6eeff98f5af246eee17a7e9fc44b0eb2b2
Пользователь <122448202+ArsamakocAslan194164@users.noreply.github.com>
2024-09-24 16:03:58 +0300

    12

---
+++ task1.py
@@ -0,0 +1,20 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99 > 9.98)
+print(1000 != 1000.1)
+
+#3rd program
+a = 2*2+2
+b = 2*(2+2)
+print(2*2+2)
+print(2*(2+2))
+print(a == b)
+
+#4th program
+a = str(123.456)
+print(a[4])
+a = float(a)
+a = a*10
+print(a)
